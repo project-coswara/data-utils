3beb330fd5efd6ffd5e1b4da23b5b4c4ccf9fc27
Prashant Krishnan <gillyprash29@gmail.com>
2020-04-17 17:19:32 +0530

    Added make_csv

--- make_csv.py
+++ make_csv.py
@@ -1,23 +1,16 @@
 import os
 import json
-# import numpy as np
-import csv
+import pandas as pd
 from pdb import set_trace as bp
 
-data_path = 'data/20200416'
+data_path = 'data/20200415'
+dest_path = 'data/20200415/20200415.csv'
 list_of_ids = os.listdir(data_path)
-data_info = open('data/20200416/20200416.csv','w')
-csvwriter = csv.writer(data_info)
-c = 0
+df = pd.DataFrame()
 for x in list_of_ids:
     with open(os.path.join(data_path,x,"metadata.json")) as f:
         metadata = json.load(f)
-        if not c:
-            header = list(metadata.keys())
-            csvwriter.writerow(header)
-            c = 1
-        csvwriter.writerow(list(metadata.values()))
-data_info.close()
-            
-
-
+        metadata_pd = [metadata]
+        df_meta = pd.DataFrame(metadata_pd)
+        df = df.append(df_meta,ignore_index=True)
+df.to_csv(dest_path,index=False)
