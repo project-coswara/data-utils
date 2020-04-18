5309c48620e09e698372368c41461375d3d7c9ca
Prashant Krishnan <gillyprash29@gmail.com>
2020-04-18 12:11:21 +0530

    Updated filter_ids

--- filter_ids.py
+++ filter_ids.py
@@ -1,19 +1,25 @@
 import os
-import subprocess
 from pdb import set_trace as bp
-data_path = '/Users/prashant/Downloads/Coswara/data/20200416'
+import pandas as pd
+
+data_path = '/Users/prashant/Downloads/Coswara/Coswara-data/'
+dates = os.listdir(data_path)
+ids_done = []
+for x in dates:
+    folder_name = os.path.join(data_path,x)
+    if os.path.isdir(folder_name):
+        df = pd.read_csv(os.path.join(folder_name,str(x)+".csv"))
+        ids_done.extend(df['id'].to_list())
 dest_path = '/Users/prashant/Downloads/Coswara/to_upload/20200416/'
-ids_file = open('/Users/prashant/Downloads/ids_20200415.txt','r')
 with open(os.path.join(data_path,'pull_data.sh','r')) as f:
     pull_file = f.read().splitlines()
 new_pull_file = open('pull_data_new.sh','w')
-list_of_ids_done = ids_file.read().splitlines()
 c1=0
 c2=0
 for x in pull_file:
     cmd_split = x.split(' ')
     ids = cmd_split[4].split('/')[4]
-    if ids in list_of_ids_done:
+    if ids in ids_done:
         c1+=1
         continue
     cmd_split[5] = dest_path
